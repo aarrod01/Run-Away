6c3148a15ff66a61fa6e7b6e8573c419265b3cc1
BlanFont <37364346+BlanFont@users.noreply.github.com>
2018-03-16 06:32:42 +0100

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	Rigidbody2D playerRB;
+
+	public int playerHealth = 1;
+	public int enemyDemage =1;
+	// Use this for initialization
+	void Start () {
+		playerRB = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is alled once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemigo") {
+			playerHealth = enemyDemage - playerHealth;
+			if (playerHealth <= 0) {
+				Destroy (gameObject);
+			}
+
+		}
+	}
+}
