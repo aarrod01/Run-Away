107517b510a647a24dfecffcca6003771732c3b8
aarrod01 <aarrod01@ucm.es>
2018-04-13 16:49:51 +0200

    Patrones Laberinto
    El script busqueda de patrones asociado al tilemap de paredes genera los
    puntos de ruta del mapa en las intersecciones del laberinto.

--- Run Away/Assets/Scripts/Recorrido/BusquedaDePatrones.cs
+++ Run Away/Assets/Scripts/Recorrido/BusquedaDePatrones.cs
@@ -24,67 +24,124 @@ namespace Patron
             }
         }
 
-        public void PatronX(Direccion[] direccionesAbiertas)
+        /*public void PatronX(Direccion[] direccionesAbiertas)
         {
 
             RellenarCentro();
             for (int i = 0; i < direccionesAbiertas.Length; i++)
                 RellenarDireccion(direccionesAbiertas[i]);
-        }
+        }*/
         public Contenido ContenidoS(int x, int y)
         {
             return mascara[x, y];
         }
-        void RellenarDireccion(Direccion direccion)
+
+        public void RellenarFines(int numero, Contenido contenido)
         {
-            switch (direccion)
+            numero = numero % 4;
+            RellenarSemiLaterales(numero, contenido);
+            Vector2Int[] x = new Vector2Int[2] { new Vector2Int(1, numero%2*3),
+                new Vector2Int(2, numero%2*3) };
+            if(numero>1)
             {
-                case Direccion.Arriba:
-                    mascara[1, 0] = Contenido.Vacio;
-                    mascara[2, 0] = Contenido.Vacio;
-                    if (mascara[1, 3] == Contenido.Desconocido)
-                    {
-                        mascara[1, 3] = Contenido.Lleno;
-                        mascara[2, 3] = Contenido.Lleno;
-                    }
+                for (int i = 0; i < x.Length; i++)
+                    x[i] = InvertirElementos(x[i]);
+            }
+            RellenarCasillas(x, contenido);
+
+        }
+        public void RellenarEntreIntersecciones(int numero, Contenido contenido)
+        {
+            numero = numero % 4;
+            Vector2Int[] x = new Vector2Int[4] { new Vector2Int(numero % 2 * 3, 0),
+                new Vector2Int(numero % 2 * 3, 1),
+                new Vector2Int((numero + 1) % 2 * 3, 2),
+                new Vector2Int((numero + 1) % 2 * 3, 3) };
+            
+            if(numero>1)
+            {
+                for (int i = 0; i < x.Length; i++)
+                    x[i] = InvertirElementos(x[i]);
+            }
+
+            RellenarCasillas(x, contenido);
+        }
+        Vector2Int InvertirElementos(Vector2Int x)
+        {
+            Vector2Int aux = new Vector2Int();
+            aux.y = x.x;
+            aux.x = x.y;
+            return aux;
+        }
+        public void RellenarSemiLaterales(int numero, Contenido contenido)
+        {
+            numero = numero % 4;
+            switch (numero)
+            {
+                case 0:
+                    RellenarCasilla(new Vector2Int(0, 1), contenido);
+                    RellenarCasilla(new Vector2Int(3, 1), contenido);
                     break;
-                case Direccion.Derecha:
-                    mascara[3, 1] = Contenido.Vacio;
-                    mascara[3, 2] = Contenido.Vacio;
-                    if (mascara[0, 1] == Contenido.Desconocido)
-                    {
-                        mascara[0, 1] = Contenido.Lleno;
-                        mascara[0, 2] = Contenido.Lleno;
-                    }
+                case 1:
+                    RellenarCasilla(new Vector2Int(0, 2), contenido);
+                    RellenarCasilla(new Vector2Int(3, 2), contenido);
                     break;
-                case Direccion.Abajo:
-                    mascara[1, 3] = Contenido.Vacio;
-                    mascara[2, 3] = Contenido.Vacio;
-                    if (mascara[1,0] == Contenido.Desconocido)
-                    {
-                        mascara[1, 0] = Contenido.Lleno;
-                        mascara[2, 0] = Contenido.Lleno;
-                    }
+                case 2:
+                    RellenarCasilla(new Vector2Int(1, 0), contenido);
+                    RellenarCasilla(new Vector2Int(1, 3), contenido);
                     break;
-                case Direccion.Izquierda:
-                    mascara[0, 1] = Contenido.Vacio;
-                    mascara[0, 2] = Contenido.Vacio;
-                    if (mascara[3, 1] == Contenido.Desconocido)
-                    {
-                        mascara[3, 1] = Contenido.Lleno;
-                        mascara[3, 2] = Contenido.Lleno;
-                    }
+                case 3:
+                    RellenarCasilla(new Vector2Int(2, 0), contenido);
+                    RellenarCasilla(new Vector2Int(2, 3), contenido);
                     break;
             }
         }
 
-        public void RellenarCentro()
+        public void RellenarEsquina(int esquina, Contenido contenido)
+        {
+            Vector2Int[] total = new Vector2Int[5];
+            total[0] = new Vector2Int((esquina % 2) * 3, (esquina / 2 * 3));
+            if(total[0].x==0)
+            {
+                for (int i = 1; i < 3; i++)
+                    total[i] = total[0] + Vector2Int.right * i;
+            }
+            else
+            {
+                for (int i = 1; i < 3; i++)
+                    total[i] = total[0] - Vector2Int.right * i;
+            }
+            if (total[0].y == 0)
+            {
+                for (int i = 3; i < 5; i++)
+                    total[i] = total[0] + Vector2Int.up * (i-2);
+            }
+            else
+            {
+                for (int i = 3; i < 5; i++)
+                    total[i] = total[0] - Vector2Int.up * (i-2);
+            }
+            RellenarCasillas(total, contenido);
+        }
+
+        public void RellenarCasillas(Vector2Int[]pos, Contenido contenido)
+        {
+            for(int i=0; i<pos.Length;i++)
+            {
+                RellenarCasilla(pos[i], contenido);
+            }
+        }
+        public void RellenarCasilla(Vector2Int pos, Contenido contenido)
+        {
+            mascara[pos.x, pos.y] = contenido;
+        }
+        public void RellenarCentro(Contenido contenido)
         {
             for (int i = 1; i < tamanyo - 1; i++)
             {
                 for (int j = 1; j < tamanyo - 1; j++)
                 {
-                    mascara[i, j] = Contenido.Vacio;
+                    mascara[i, j] = contenido;
                 }
             }
         }
@@ -100,14 +157,18 @@ public class BusquedaDePatrones : MonoBehaviour
 
     TileBase[] casillas;
     BoundsInt limite;
-    Patron4[] patrones;
+    Patron4[] patronesGenerales, patronesImposibles, patronesExcepciones;
     Vector2 posicionSuperiorIzquierda;
     Vector2 vectorDesdeEsquinaSuperiorIzquierdaACentro;
     float ancho;
+    GameObject padre;
+
     void Awake()
     {
 
-        patrones = TodosLosPatrones();
+        patronesGenerales = PatronGeneral();
+        patronesImposibles = PatronExcluyente();
+        patronesExcepciones = PatronExcepciones();
         Tilemap aux = GetComponent<Tilemap>();
         limite = aux.cellBounds;
         posicionSuperiorIzquierda = aux.LocalToWorld(new Vector2(aux.localBounds.min.x, aux.localBounds.min.y));
@@ -117,49 +178,45 @@ public class BusquedaDePatrones : MonoBehaviour
         CrearEnCentroPatron(prefab);
     }
 
-    Patron4[] TodosLosPatrones()
-    {/*
-        Patron4[] ret = new Patron4[1];
+    Patron4[] PatronGeneral()
+    {
+        Patron4[] ret = new Patron4[5];
         ret[0] = new Patron4();
-            ret[0].RellenarCentro();
-        */
-        Patron4[] ret = new Patron4[13];
-        int i = 0;
-        Direccion[] direcciones = new Direccion[1];
-        while (i < 4)
+            ret[0].RellenarCentro(Contenido.Vacio);
+        return ret;
+    }
+    Patron4[] PatronExcluyente()
+    {
+        Patron4[] ret = new Patron4[12];
+        for (int i = 0; i < 4; i++)
         {
-            direcciones[0] = (Direccion)(i % 4);
             ret[i] = new Patron4();
-            ret[i].PatronX(direcciones);
-            i++;
+            ret[i].RellenarEsquina(i, Contenido.Vacio);
+
         }
-        direcciones = new Direccion[2];
-        while (i < 8)
+        for(int i = 4;i<8;i++)
         {
-            direcciones[0] = (Direccion)(i % 4);
-            direcciones[1] = (Direccion)((i + 1) % 4);
             ret[i] = new Patron4();
-            ret[i].PatronX(direcciones);
-            i++;
+            ret[i].RellenarSemiLaterales(i, Contenido.Lleno);
         }
-        direcciones = new Direccion[3];
-        while (i < 12)
+        for (int i = 8; i < 12; i++)
         {
-            direcciones[0] = (Direccion)(i % 4);
-            direcciones[1] = (Direccion)((i + 1) % 4);
-            direcciones[1] = (Direccion)((i + 2) % 4);
             ret[i] = new Patron4();
-            ret[i].PatronX(direcciones);
-            i++;
+            ret[i].RellenarEntreIntersecciones(i, Contenido.Vacio);
         }
-        direcciones = new Direccion[4];
-        direcciones[0] = Direccion.Abajo;
-        direcciones[1] = Direccion.Arriba;
-        direcciones[2] = Direccion.Derecha;
-        direcciones[3] = Direccion.Izquierda;
-        ret[i] = new Patron4();
-        ret[i].PatronX(direcciones);
-        
+
+
+        return ret;
+    }
+
+    Patron4[] PatronExcepciones()
+    {
+        Patron4[] ret = new Patron4[4];
+        for (int i = 0; i < 4; i++)
+        {
+            ret[i] = new Patron4();
+            ret[i].RellenarFines(i, Contenido.Lleno);
+        }
         return ret;
     }
 
@@ -183,24 +240,38 @@ public class BusquedaDePatrones : MonoBehaviour
         return fin;
     }
 
-    bool EstaAlgunPatronEn(int x, int y)
+    bool EvaluarPatrones(int x, int y)
     {
         int i = 0;
-        while (i < patrones.Length && !EstaElPatronEn(x, y, patrones[i]))
+        while (i < patronesImposibles.Length && !EstaElPatronEn(x, y, patronesImposibles[i]))
             i++;
-        return i != patrones.Length;
+        if (i == patronesImposibles.Length)
+        {
+            i = 0;
+            while (i < patronesGenerales.Length && !EstaElPatronEn(x, y, patronesGenerales[i]))
+                i++;
+            return i != patronesGenerales.Length;
+        }
+        else
+        {
+            i = 0;
+            while (i < patronesExcepciones.Length && !EstaElPatronEn(x, y, patronesExcepciones[i]))
+                i++;
+            return i < patronesExcepciones.Length; ;
+        }
     }
 
     public void CrearEnCentroPatron(GameObject prefab)
     {
-        GameObject padre = new GameObject();
+        padre = new GameObject();
         padre.name = "Puntos Ruta";
         for (int i = 0; i < limite.size.x-3; i++)
         {
             for (int j = 0; j < limite.size.y-3; j++)
             {
-                if (EstaAlgunPatronEn(i, j))
+                if (EvaluarPatrones(i, j))
                 {
+                    
                     GameObject a=
                     Instantiate(prefab, posicionSuperiorIzquierda + Vector2.right * i*ancho + Vector2.up * j*ancho + vectorDesdeEsquinaSuperiorIzquierdaACentro, Quaternion.identity);
                     a.transform.parent = padre.transform;
