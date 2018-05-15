99d454b0b63a0d0621e015fcd86ac91642592bc8
aarrod01 <aarrod01@ucm.es>
2018-05-15 21:32:22 +0200

    Añadido menu de opciones y añadido audio mixer

--- Run Away/Assets/Scripts/Menus/MenuPausa.cs
+++ Run Away/Assets/Scripts/Menus/MenuPausa.cs
@@ -6,10 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour {
 
-	public Canvas cv;
+	public GameObject panelPausa;
+	public GameObject panelOpciones;
 
 	void Update(){
-        if (Input.GetKeyDown("p"))
+		if (Input.GetKeyDown("p")  || Input.GetKeyDown(KeyCode.Escape))
             PausarPartida();
         else if (Input.GetKeyDown(KeyCode.Escape)&&Time.timeScale==0.0f)
             SalirAlMenu();
@@ -19,19 +20,24 @@ public class MenuPausa : MonoBehaviour {
 	public void PausarPartida(){
 		if (Time.timeScale == 1.0f) {
 			Time.timeScale = 0.0f;
-			cv.gameObject.SetActive (true);
+			panelPausa.SetActive (true);
 			Debug.Log (Application.persistentDataPath);
 			Debug.Log (Application.dataPath);
 		} else {
 			Time.timeScale = 1.0f;
-			cv.gameObject.SetActive (false);
+			panelPausa.SetActive (false);
 		}
+	}
+
+	public void MenuOpciones (){
+		panelPausa.SetActive (false);
+		panelOpciones.SetActive (true);
 	}
 
 	public void SalirAlMenu (){
 		//Guardar
 		Time.timeScale = 1.0f;
-		cv.gameObject.SetActive (false);
+		panelPausa.SetActive (false);
 		SaveLoadManager.SaveGame(GameManager.instance);
         GameManager.instance.CambiarEscena("Inicio");
 	}
---
+++ Run Away/Assets/SoundMixer/VolumeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour {
+
+	public AudioMixer mixer;
+
+	public void ChangeOverallVol (float vol){
+		mixer.SetFloat ("OverallVolume", Mathf.Log10 (vol) * 20f);
+	}
+
+	public void ChangeMusiclVol (float vol){
+		mixer.SetFloat ("MusicVol", Mathf.Log10 (vol) * 20f);
+	}
+
+	public void ChangeFxlVol (float vol){
+		mixer.SetFloat ("FxVolume", Mathf.Log10 (vol) * 20f);
+	}
+}
