a5dcd9564b70704b03b6aeb1d364e4fb95601762
aarrod01 <aarrod01@ucm.es>
2018-05-17 21:19:43 +0200

    Probando efectos post procces

--- Run Away/Assets/Resources/SoundMixer/VolumeControl.cs
+++ Run Away/Assets/Resources/SoundMixer/VolumeControl.cs
@@ -5,16 +5,21 @@ using UnityEngine.Audio;
 public class VolumeControl : MonoBehaviour {
 
 	public AudioMixer mixer;
+	public PostEffectsBase peb;
 
 	public void ChangeOverallVol (float vol){
 		mixer.SetFloat ("OverallVolume", Mathf.Log10 (vol) * 20f);
 	}
 
 	public void ChangeMusiclVol (float vol){
-		mixer.SetFloat ("MusicVol", Mathf.Log10 (vol) * 20f);
+		mixer.SetFloat ("MusicVolume", Mathf.Log10 (vol) * 20f);
 	}
 
 	public void ChangeFxlVol (float vol){
 		mixer.SetFloat ("FxVolume", Mathf.Log10 (vol) * 20f);
+	}
+
+	public void ChangeBright (float bright){
+
 	}
 }
