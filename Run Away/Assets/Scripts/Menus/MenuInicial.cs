e3da012da7b9b166115fdcc4b200465a556125d8
Lebami <36412012+Lebami@users.noreply.github.com>
2018-05-20 20:56:31 +0200

    Modificado menu principal, valores..

--- Run Away/Assets/Scripts/Menus/MenuInicial.cs
+++ Run Away/Assets/Scripts/Menus/MenuInicial.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuInicial : MonoBehaviour{
+public class MenuInicial : MonoBehaviour
+{
+    public GameObject menuInicial;
+    public GameObject panelOpciones;
 
+    void Awake()
+    {
+        menuInicial.SetActive(true);
+    }
 
 	public void NuevaPartida(){
 		GameManager.instance.CambiarEscena("Nivel1");
@@ -25,6 +32,18 @@ public class MenuInicial : MonoBehaviour{
 		}
 		//SceneManager.LoadScene (nivel.ToString());
 	}
+
+    public void EntrarOpciones()
+    {
+        menuInicial.SetActive(false);
+        panelOpciones.SetActive(true);
+    }
+
+    public void SalirOpciones()
+    {
+        panelOpciones.SetActive(false);
+        menuInicial.SetActive(true);
+    }
 
 	public void SalirDelJuego(){
 		Application.Quit ();
