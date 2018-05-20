3de8247a9089a61f304b8bc75a665ed2a2902840
CarlosDuranDominguez <carduran@ucm.es>
2018-05-20 18:23:24 +0200

    Cambio gestor sonido
    Ahora el volumen de los mixers se maneja mejor desde los menús.

--- Run Away/Assets/Scripts/Controlador/SoundManager.cs
+++ Run Away/Assets/Scripts/Controlador/SoundManager.cs
@@ -48,40 +48,48 @@ public class SoundManager : MonoBehaviour {
     {
     }
 
+    
+
     public float PorcentajeVolumenGlobal()
     {
         float aux;
         mainMixer.GetFloat("OverallVolume", out aux);
-        return aux - volumenPredeterminado;
+        return aux<volumenPredeterminado ? (aux-volumenPredeterminado)/(80f+volumenPredeterminado): 
+            (aux - volumenPredeterminado) / (20f - volumenPredeterminado);
     }
 
     public void CambiarVolumenGlobal(float porcentaje)
     {
-        mainMixer.SetFloat("OverallVolume", porcentaje == 0 ? float.MinValue : volumenPredeterminado + porcentaje);
+        mainMixer.SetFloat("OverallVolume", porcentaje > 0f ? volumenPredeterminado + porcentaje*(20f-volumenPredeterminado) 
+            : volumenPredeterminado - porcentaje * (-80f - volumenPredeterminado));
     }
 
     public float PorcentajeVolumenMusica()
     {
         float aux;
         mainMixer.GetFloat("MusicVolume", out aux);
-        return aux - volumenMusicaPredeterminado;
+        return aux < volumenMusicaPredeterminado ? (aux - volumenMusicaPredeterminado) / (80f + volumenMusicaPredeterminado) : 
+            (aux - volumenMusicaPredeterminado) / (20f - volumenMusicaPredeterminado);
     }
 
     public void CambiarVolumenMusica(float porcentaje)
     {
-        mainMixer.SetFloat("MusicVolume", porcentaje == 0 ? float.MinValue : volumenMusicaPredeterminado + porcentaje );
+        mainMixer.SetFloat("MusicVolume", porcentaje > 0f ? volumenMusicaPredeterminado + porcentaje * (20f - volumenMusicaPredeterminado)
+            : volumenMusicaPredeterminado - porcentaje * (-80f - volumenMusicaPredeterminado));
     }
 
     public float PorcentajeVolumenEfectos()
     {
         float aux;
         mainMixer.GetFloat("FxVolume", out aux);
-        return aux - volumenSonidosPredeterminado;
+        return aux < volumenSonidosPredeterminado ? (aux - volumenSonidosPredeterminado) / (80f + volumenSonidosPredeterminado) :
+            (aux - volumenSonidosPredeterminado) / (20f - volumenSonidosPredeterminado);
     }
 
     public void CambiarVolumenEfectos(float porcentaje)
     {
-        mainMixer.SetFloat("FxVolume", porcentaje == 0 ? float.MinValue : volumenSonidosPredeterminado+porcentaje);
+        mainMixer.SetFloat("FxVolume", porcentaje > 0f ? volumenSonidosPredeterminado + porcentaje * (20f - volumenSonidosPredeterminado)
+            : volumenSonidosPredeterminado - porcentaje * (-80f - volumenSonidosPredeterminado));
     }
 
     public void CambiarTonoMusica(float porcentaje)
